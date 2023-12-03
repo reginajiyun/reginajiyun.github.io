f31fccf22229ad5637c04705368cb6aadd01f26c
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2023-12-03 04:41:50 -0500

    commit
    commit for harmonic collection

---
+++ entries/entry2/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nextButton = document.getElementById('nextButton');
+  
+  });
+  
--- entries/entry3/main.js
+++
@@ -1,5 +0,0 @@
-console.log("hello!");
-
-
-let navElement = document.querySelector(".nav");
-console.log("my nav element:", navElement);
