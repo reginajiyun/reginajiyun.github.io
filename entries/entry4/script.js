176d630c9340e5665ffec1b303177757e6a1abf4
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2023-12-04 17:40:44 -0500

    commit
    commit for harmonic collection

---
+++ entries/entry4/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const whereText = document.getElementById('whereText');
+    const withWhoText = document.getElementById('withWhoText');
+    const nextButton = document.getElementById('nextButton');
+  
+    withWhoText.addEventListener('click', function() {
+      window.open('https://www.instagram.com/evelyn__yoon/', '_blank');
+    });
+  
+    nextButton.addEventListener('click', function() {
+      window.location.href = 'https://reginajiyun.github.io/entries/entry5/index.html';
+    });
+  });
+  
