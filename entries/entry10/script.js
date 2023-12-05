16954f3a04465f23b4af0c53e1f0b2592487a691
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2023-12-05 18:31:24 -0500

    commit

---
+++ entries/entry10/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nextButton = document.getElementById('nextButton');
+  
+    nextButton.addEventListener('click', function() {
+      window.location.href = 'https://reginajiyun.github.io/entries/entry11/index.html';
+    });
+  });
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
