fbd2faca7f3165f357c89bebde5bbbc10365d523
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2023-12-05 17:19:01 -0500

    commit
    commit for harmonic collection

---
+++ entries/entry7/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nextButton = document.getElementById('nextButton');
+  
+    nextButton.addEventListener('click', function() {
+      window.location.href = 'https://reginajiyun.github.io/entries/entry8/index.html';
+    });
+  });
