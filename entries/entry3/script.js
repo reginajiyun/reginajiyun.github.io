1af498ef5b06f00d4bdd7e8c6102a82d4d34382c
reginajiyun <148292294+reginajiyun@users.noreply.github.com>
2023-12-05 16:41:50 -0500

    commit
    commit for harmonic collection

--- entries/entry3/script.js
+++ entries/entry3/script.js
@@ -1,14 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
   const nextButton = document.getElementById('nextButton');
-  const imageUrls = [
-      'image1.jpg',
-      'image2.jpg',
-      'image3.jpg',
-      'image4.jpg'
-    ];
-  
-    for (let i = 0; i < imageUrls.length; i++) {
-      const imageElement = document.getElementById(`image${i + 1}`);
-      imageElement.style.backgroundImage = `url(${imageUrls[i]})`;
-    }
   });
---
+++ entries/entry5/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const withWhoText = document.getElementById('withWhoText');
+    const nextButton = document.getElementById('nextButton');
+  
+    withWhoText.addEventListener('click', function() {
+      window.location.href = 'https://reginajiyun.github.io/entries/entry6/index.html';
+    });
+  });
+  
